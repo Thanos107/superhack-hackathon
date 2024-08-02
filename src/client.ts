117d712bd15846743aac3f78f7af2f7be4d52e00
Thanos107 <gauravmoona007@gmail.com>
2024-08-02 08:39:34 +0530

    first-commit

--- src/client.ts
+++ src/client.ts
@@ -2,8 +2,8 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
-
+// const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
+const clientId = "2dcda4b39b2d62de8ec249c5d208c530";
 export const client = createThirdwebClient({
   clientId: clientId,
 });
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+
+
+// client-id= "2dcda4b39b2d62de8ec249c5d208c530"
+// secret-id = "TaTcALwiU8-ykW2CPseBWxtmV3yxbmpAfcdLjG7wo-VUtdvtvVAMKguMBengEzCN4k-dpKd3jqY3vFkw_1Ru0w "
